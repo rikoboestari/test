18ee40ea5c6d1b6583526887e01df48cb02ea653
boes <riko.boestari@tokopedia.com>
2017-02-28 17:45:11 +0700

    first commit

---
+++ app.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+func createPoint(x int, y int) *Point {
+	return &Point{
+		X: x,
+		Y: y,
+	}
+}
+
+func calculateDistance(a *Point, b *Point) float64 {
+	return math.Sqrt(math.Pow(float64(a.X - b.X), 2) + math.Pow(float64(a.Y - b.Y), 2))
+}
+
+func main() {
+	pointA := createPoint(0, 6)
+	var pointB *Point
+	fmt.Println(calculateDistance(pointA, pointB))
+	fmt.Println("done!")
+}
---
+++ src/aservice/talk.go
@@ -0,0 +1,24 @@
+package aservice
+
+//import "time"
+
+type Talk struct {
+	talkId int
+	shopId int
+	productId int
+	//userId int
+	//status int
+	//message string
+	//totalComment int
+	//lastCommentBy int
+	//lastCommentTime int
+	//createBy int
+	//createTime time.Time
+	//updateBy int
+	//updateTime time.Time
+}
+
+func GetLatestTalk(howMany int) []Talk{
+
+	return nil
+}
